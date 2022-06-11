224ffcfaa9675ab4498a30ef045a7142c4ffebdd
Josh Justice <me@codingitwrong.com>
2022-06-11 14:08:41 -0400

    Test specific domain

--- spec/requests/links_spec.rb
+++ spec/requests/links_spec.rb
@@ -3,15 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe 'links', type: :request do
-  it 'redirects to the link and logs the hit' do
-    link = FactoryBot.create(:link)
+  context 'with a link with no domain' do
+    it 'redirects to the link and logs the hit' do
+      link = FactoryBot.create(:link)
 
-    get "/#{link.slug}"
+      get "/#{link.slug}"
 
-    expect(response.status).to eq(302)
-    expect(response.header['Location']).to eq(link.destination)
+      expect(response.status).to eq(302)
+      expect(response.header['Location']).to eq(link.destination)
 
-    expect(Hit.count).to eq(1)
-    expect(Hit.last.link).to eq(link)
+      expect(Hit.count).to eq(1)
+      expect(Hit.last.link).to eq(link)
+    end
+  end
+
+  context 'with a link with a matching domain' do
+    it 'redirects to the link and logs the hit' do
+      link = FactoryBot.create(:link, domain: 'www.example.com')
+
+      get "/#{link.slug}"
+
+      expect(response.status).to eq(302)
+      expect(response.header['Location']).to eq(link.destination)
+    end
   end
 end
